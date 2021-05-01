aaea787615b63baa5ba193c6a21fa0a54579de16
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2021-05-01 10:17:54 +0300

    the repo grid looks great

--- src/api.js
+++ src/api.js
@@ -3,13 +3,14 @@ export function fetch_most_popular (language) {
     const query = "stars:>1" + (language ? " language:" + encodeURIComponent(language) : "")
     const endpoint = `https://${host}/search/repositories?q=${query}&sort=stars&order=desc`
 
+    // console.log(endpoint)
+
     return fetch(endpoint)
         .then((response) => response.json())
         .then((data) => {
             if (!data.items) {
                 throw new Error(data.message)
             }
-            let results = data.items.map(x => ({url: x.url, stars: x.stargazers_count}))
-            return results
+            return data.items
         })
 }
--- src/popular.js
+++ src/popular.js
@@ -1,6 +1,8 @@
 import React from "react"
-
+import PropTypes from "prop-types"
 import {fetch_most_popular} from "./api"
+import {FaUser, FaStar, FaCodeBranch, FaExclamationTriangle} from "react-icons/fa"
+
 
 function LanguagesNav ({selected, onSelectLanguage}) {
     const languages = ["All", "Python", "Ruby", "Haskell", "F#", "Clojure", "Elm", "Lisp", "JavaScript"]
@@ -19,6 +21,42 @@ function LanguagesNav ({selected, onSelectLanguage}) {
         </ul>
     )
 }
+LanguagesNav.propTypes = {
+    selected: PropTypes.string.isRequired,
+    onSelectLanguage: PropTypes.func.isRequired,
+}
+
+
+function RepositoriesGrid ({repos}) {
+    return (
+        <ul className="grid space-around">
+            {repos.map((repo, index) => {
+                const {name, owner, html_url, stargazers_count, forks, open_issues} = repo
+                const {login, avatar_url} = owner
+
+                return (
+                    <li key={html_url} className="repo bg-light">
+                        <h4 className="header-lg center-text">#{index + 1}</h4>
+                        <img className="avatar" src={avatar_url} alt={`${login}'s avatar.`}/>
+                        <h2 className="center-text">
+                            <a className="link" href={html_url}>{name}</a>
+                        </h2>
+                        <ul className="repo-cards">
+                            <li><FaUser color="rgb(255, 191, 116)" size={22}/>{login}</li>
+                            <li><FaStar color="rgb(255, 215, 0)" size={22}/>{stargazers_count.toLocaleString()} stars</li>
+                            <li><FaCodeBranch color="rgb(129, 195, 245)" size={22}/>{forks.toLocaleString()} forks</li>
+                            <li><FaExclamationTriangle color="rgb(241, 138, 147)" size={22}/>{open_issues.toLocaleString()} issues</li>
+                        </ul>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
+RepositoriesGrid.propTypes = {
+    repos: PropTypes.array.isRequired,
+}
+
 
 class Popular extends React.Component {
     constructor(props) {
@@ -45,7 +83,6 @@ class Popular extends React.Component {
             error_message: null,
         })
         if (!repos[new_selection]) {
-            console.log("fetching", new_selection)
             fetch_most_popular(new_selection)
                 .then((data) => this.setState({
                         selected: new_selection,
@@ -57,9 +94,6 @@ class Popular extends React.Component {
                     })
                 )
                 .catch((error) => this.setState({error_message: error.message}))
-        }
-        else {
-            console.log(new_selection, repos[new_selection])
         }
     }
 
@@ -81,7 +115,7 @@ class Popular extends React.Component {
 
                 {error_message && <p className="error-message">Houston, we've got a problem: {error_message}</p>}
 
-                {repos[selected] && <pre>{JSON.stringify(repos[selected], null, 4)}</pre>}
+                {repos[selected] && <RepositoriesGrid repos={repos[selected]} />}
             </React.Fragment>
         )
     }
